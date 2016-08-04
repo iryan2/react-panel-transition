2eba268456aaf7c8116c4c6dc1dd7954496671cd
Ian Ryan <iryan2@gmail.com>
2016-08-04 12:08:44 +1000

    Correctly configure onEnter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { TransitionMotion, spring } from 'react-motion';
+import _ from 'lodash';
 import './App.css';
 
 const LeftPanel = props => (
   <div className="panel left" style={props.style}>
     <h4>Left</h4>
+    <button onClick={() => props.onClick('left')}>
+      Toggle left panel
+    </button>
+    <button onClick={() => props.onClick('middle')}>
+      Toggle middle panel
+    </button>
+    <button onClick={() => props.onClick('right')}>
+      Toggle right panel
+    </button>
   </div>
 );
 
@@ -13,6 +23,9 @@ const MiddlePanel = props => (
     <h4>Middle</h4>
     <button onClick={() => props.onClick('left')}>
       Toggle left panel
+    </button>
+    <button onClick={() => props.onClick('middle')}>
+      Toggle middle panel
     </button>
     <button onClick={() => props.onClick('right')}>
       Toggle right panel
@@ -23,12 +36,28 @@ const MiddlePanel = props => (
 const RightPanel = props => (
   <div className="panel right" style={props.style}>
     <h4>Right</h4>
+    <button onClick={() => props.onClick('left')}>
+      Toggle left panel
+    </button>
+    <button onClick={() => props.onClick('middle')}>
+      Toggle middle panel
+    </button>
     <button onClick={() => props.onClick('right')}>
-      Close right panel
+      Toggle right panel
     </button>
   </div>
 );
 
+const panelConfig = [{
+  key: 'left',
+  component: LeftPanel,
+}, {
+  key: 'middle',
+  component: MiddlePanel,
+}, {
+  key: 'right',
+  component: RightPanel,
+}];
 
 class App extends Component {
   constructor(props) {
@@ -36,41 +65,49 @@ class App extends Component {
 
     // set the initial state
     this.state = {
-      panels: [{
-        key: 'left',
-        component: LeftPanel,
-      }, {
-        key: 'middle',
-        component: MiddlePanel,
-      }, {
-        key: 'right',
-        component: RightPanel,
-      }],
+      panels: panelConfig,
       offset: 0,
       left: true,
       middle: true,
       right: true,
     };
 
-
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(key) {
-    this.setState({
-      [key]: !this.state[key],
-      panels: [{
-        key: 'middle',
-        component: MiddlePanel,
-      }, {
-        key: 'right',
-        component: RightPanel,
-      }],
-    });
+    const { panels: activePanels } = this.state;
+    // If choice is not active, add it
+    const panelIsActive = _.findIndex(activePanels, { key }) !== -1;
+
+    if (panelIsActive) {
+      // Remove selected panel
+      const otherPanels = _.filter(activePanels, item => item.key !== key);
+      this.setState({
+        [key]: !this.state[key],
+        panels: otherPanels,
+      });
+    } else {
+      // Add selected panel
+      const selectedPanel = _.find(panelConfig, { key });
+      const currentPanels = _.clone(this.state.panels);
+      const originalIndex = _.findIndex(panelConfig, { key });
+
+      currentPanels.splice(originalIndex, 0, selectedPanel);
+
+      this.setState({
+        [key]: !this.state[key],
+        panels: currentPanels,
+      });
+    }
   }
 
   willLeave() {
     return { left: spring(-400) };
+  }
+
+  willEnter(styleThatEntered) {
+    return { left: -400 };
   }
 
   render() {
@@ -81,11 +118,12 @@ class App extends Component {
 
         <TransitionMotion
           willLeave={this.willLeave}
+          willEnter={this.willEnter}
           styles={this.state.panels.map(({ key, component }) => {
             return ({
               key,
               data: { component },
-              style: { left: this.state.offset },
+              style: { left: spring(this.state.offset) },
             })
           })}>
 
